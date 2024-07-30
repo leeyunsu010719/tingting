6f8bf5aa3c9c8ba9165d4322633f5a69162abc3b
leeyunsu010719 <leeyunsu010719@gmail.com>
2024-07-30 23:57:12 +0900

    Add files via upload

---
+++ js/slide_script.js
@@ -0,0 +1,22 @@
+// slide
+setInterval(function(){
+    $("#banner ul").delay(4000);
+    $("#banner ul").animate({
+        marginLeft: '-1920px'
+    },500)
+    $("#banner ul").delay(4000);
+    $("#banner ul").animate({
+        marginLeft: '-3840px'
+    },500)
+    $("#banner ul").delay(4000);
+    $("#banner ul").animate({
+        marginLeft: '-5760px'
+    },500)
+    $("#banner ul").delay(4000);
+    $("#banner ul").animate({
+        marginLeft: '-7680px'
+    },500)
+    $("#banner ul").animate({
+        marginLeft: '0px'
+    },0)
+});
